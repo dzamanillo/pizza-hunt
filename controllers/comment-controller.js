9a75d6c43605ce62615116a53b3811e6963487e8
Daniel Zamanillo <dzamanillok@gmail.com>
2021-10-06 11:10:49 -0500

    reply subdoc created and hooked into front end

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,7 +1,7 @@
 const { Comment, Pizza } = require("../models");
 
 const commentController = {
-	// add comment
+	// add comment to pizza
 	addComment({ params, body }, res) {
 		console.log(body);
 		Comment.create(body)
@@ -22,13 +22,29 @@ const commentController = {
 			.catch((err) => res.json(err));
 	},
 
+	// add reply to comment
+	addReply({ params, body }, res) {
+		Comment.findOneAndUpdate(
+			{ _id: params.commentId },
+			{ $push: { replies: body } },
+			{ new: true }
+		)
+			.then((dbPizzaData) => {
+				if (!dbPizzaData) {
+					res.status(404).json({ message: "No pizza found with this id!" });
+					return;
+				}
+				res.json(dbPizzaData);
+			})
+			.catch((err) => res.json(err));
+	},
+
 	// remove comment
 	removeComment({ params }, res) {
 		Comment.findOneAndDelete({ _id: params.commentId })
 			.then((deletedComment) => {
 				if (!deletedComment) {
-					res.status(404).json({ message: `No comment found with this id.` });
-					return;
+					return res.status(404).json({ message: "No comment with this id!" });
 				}
 				return Pizza.findOneAndUpdate(
 					{ _id: params.pizzaId },
@@ -38,11 +54,21 @@ const commentController = {
 			})
 			.then((dbPizzaData) => {
 				if (!dbPizzaData) {
-					res.status(404).json({ message: `No pizza found with this id.` });
+					res.status(404).json({ message: "No pizza found with this id!" });
 					return;
 				}
 				res.json(dbPizzaData);
 			})
+			.catch((err) => res.json(err));
+	},
+	// remove reply
+	removeReply({ params }, res) {
+		Comment.findOneAndUpdate(
+			{ _id: params.commentId },
+			{ $pull: { replies: { replyId: params.replyId } } },
+			{ new: true }
+		)
+			.then((dbPizzaData) => res.json(dbPizzaData))
 			.catch((err) => res.json(err));
 	},
 };
